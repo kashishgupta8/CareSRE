16443def049dfd42856ab3403e3c85aac9c586c3
R <reachtorishab03@gmail.com>
2026-01-05 19:56:05 +0000

    Try fixing this error: `Console Error: ReactDOM.useFormState has been re

--- src/components/patient-registration-form.tsx
+++ src/components/patient-registration-form.tsx
@@ -1,7 +1,8 @@
 
 'use client';
 
-import { useFormState, useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { registerPatient } from '@/app/actions';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
@@ -35,7 +36,7 @@ function SubmitButton() {
 }
 
 export function PatientRegistrationForm() {
-  const [state, formAction] = useFormState(registerPatient, initialState);
+  const [state, formAction] = useActionState(registerPatient, initialState);
   const { toast } = useToast();
 
   useEffect(() => {
